de3dd32ad1fe737df85e1238c3b25187390d984d
Euclid <eperegri@calpoly.edu>
2025-03-11 20:34:33 -0700

    fixed url to azure

--- packages/react-frontend/src/ToDoPage.jsx
+++ packages/react-frontend/src/ToDoPage.jsx
@@ -156,7 +156,7 @@ function ToDoPage({ goToTaskPage, savedToken, loginState }) {
 
 	// auth stuff
 	function loginUser(creds) {
-		const promise = fetch(`http:taskmaker-cqf6b6ded0huddhe.westus3-01.azurewebsites.net/login`, {
+		const promise = fetch(`http://taskmaker-cqf6b6ded0huddhe.westus3-01.azurewebsites.net/login`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
